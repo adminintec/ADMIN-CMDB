854aa20889585f503f95418fe2fd43deefe58736
Jonathan Taveras <jonathanrtv@outlook.com>
2019-08-24 09:58:05 -0400

    Refactor some code

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
 const menu = require("./menu")
 const Question = require("./Question")
+const clear  = require('clear');
 
 const handleCreate = () => {
-
     Question.askConfigurationItem().then(v=> {
-
-        console.log(v)
+      const { name, description, version } = v;
+        console.log(`
+          Created:
+          |
+          |
+          |___Name: ${name}
+          |
+          |___Description: ${description}
+          |
+          |___Version: ${version}
+        `)
+        setTimeout(() => {
+          clear();
+          menu.initialMenu(handleCreate, null);
+        }, 3000);
     })
 }
 
--- app/menu/index.js
+++ app/menu/index.js
@@ -1,10 +1,14 @@
 const menu = require("inquirer-menu");
-
 const chalk = require('chalk');
 const clear = require('clear');
+const figlet = require('figlet')
 
-
+// TODO: Create manage menu, ask for Upgrade and Deprecate 
 const initialMenu = (fnCreate, fnManage) => {
+  clear()
+  console.log(chalk.yellow(
+    figlet.textSync('CMDB', { horizontalLayout: 'full' })
+  ));
     const objMenu =  {
         message: 'What do you want to do, human being?',
         choices: {
