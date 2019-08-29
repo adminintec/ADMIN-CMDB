125be1c6cd6a2ad3d6a5e629d4a38e2a43314759
Jonathan Taveras <jonathanrtv@outlook.com>
2019-08-29 18:41:09 -0400

    refactor

--- app/index.js
+++ app/index.js
@@ -40,18 +40,21 @@ const createInDatabase = (objValues) => {
     [`${newKeyCreated}`]: objCreated,
   };
   const objToJson = JSON.stringify(objItems);
-  fs.writeFile(__dirname + '/Database/data.json', objToJson)
-  console.log(`
-      CI created:
-      |
-      |
-      |_name: ${name}
-      |
-      |__description: ${description}
-      |
-      |___version: ${version}
-      |__dependencies: ${dependenciesName ? dependenciesName : 'N/A'}
-    `);
+  console.log(dependenciesName)
+  fs.writeFile(__dirname + '/Database/data.json', objToJson, () => {
+    console.log(`
+    CI created:
+    |
+    |
+    |_name: ${name}
+    |
+    |__description: ${description}
+    |
+    |___version: ${version}
+    |__dependencies: ${dependenciesName ? dependenciesName : 'N/A'}
+  `);
+  })
+
 };
 
 const getDependenciesName = (objItem) => {
@@ -64,8 +67,9 @@ const getDependenciesName = (objItem) => {
 const handleTypeVersionUpdate = (objValues) => {
   const { typeVersion, objItem, newVersion } = objValues;
   const currVersion = objItem.version;
-  const majorVersion = currVersion.substring(0, 1);
-  const minorVersion = currVersion.substring(2, 3);
+  const majorVersion = currVersion.substring(0, currVersion.indexOf('.'));
+  const strBeforeMajorVersion = currVersion.substring(currVersion.indexOf('.'), currVersion.length);
+  const minorVersion = strBeforeMajorVersion.substring(1, currVersion.indexOf('.'));
   switch (typeVersion) {
     case 'Major':
       const dependenciesAffected = getDependenciesName(objItem);
@@ -103,12 +107,14 @@ const upgradeVersion = (objValues) => {
       [`${idConfItem}`]: objUpdated,
     };
     const objToJson = JSON.stringify(objItems);
-    fs.writeFile(__dirname + '/Database/data.json', objToJson)
-    console.log(`
+    fs.writeFile(__dirname + '/Database/data.json', objToJson, () => {
+      console.log(`
       Version of ${objUpdated.name}:
       |
       |__UPDATED VERSION ${objItem.version} TO ${newVersionNumber}
     `);
+    })
+
   }
 }
 const getCIWithDependencies = idConfItem => {
@@ -116,23 +122,39 @@ const getCIWithDependencies = idConfItem => {
   const confItems = Object.values(data)
   const CIdependencies = confItems.filter(items => {
     const dependenciesInCI = items.dependencies;
-    const isDependencies =  dependenciesInCI.filter(i => i === idConfItem[0])
+    const isDependencies = dependenciesInCI.filter(i => i === idConfItem[0])
 
     return isDependencies.length > 0
   })
-  return CIdependencies 
+  return CIdependencies
 }
 
 const deprecateCI = (idConfItem) => {
   const dependenciesWithConflictIfIsDeprecated = getCIWithDependencies(idConfItem)
-  dependenciesWithConflictIfIsDeprecated.forEach(item => {
+  if (dependenciesWithConflictIfIsDeprecated.length === 0) {
     console.log(`
-     This CI will be affected of ${item.name}:
-      |
-      |__With the  version ${item.version}
-    `)
-  })
-  
+       _____________________________________________________________________
+      |                                 👍🏻                                  |
+      |                                                                     |
+      | There is no dependency that could affect the deprecation of that IC |                                              
+      |_____________________________________________________________________|
+   `)
+  } else {
+    dependenciesWithConflictIfIsDeprecated.forEach(item => {
+      console.log(`
+       If you deprecate this CI, this CI will be affected:
+        |
+        |_____________________________________________
+        |                                           |_|
+        | CI Name: ${item.name}                       
+        | With the current version: ${item.version}  
+        |                                            _
+        |___________________________________________|_|
+      `)
+    })
+  }
+
+
 }
 
 
@@ -152,13 +174,13 @@ const handleCreateCI = () => {
         const dependenciesParsed = dependenciesForConfigurationItem.map(v => parseInt(v))
         createInDatabase({ ...values, dependenciesSelected: dependenciesParsed, dependenciesName })
         setTimeout(() => {
-          initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+          initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
         }, 3000);
       })
     } else {
       createInDatabase({ ...values })
       setTimeout(() => {
-        initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+        initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
       }, 3000);
     }
 
@@ -180,7 +202,7 @@ const handleListAll = () => {
     `);
     return dt
   })
-  initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+  initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
 };
 
 const handleUpgradeCI = () => {
@@ -198,7 +220,7 @@ const handleUpgradeCI = () => {
         typeVersion: typeVersion
       })
       setTimeout(() => {
-        initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+        initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
       }, 3000);
     })
 
@@ -211,14 +233,47 @@ const handleDeprecateCI = () => {
     const { dependenciesSelected } = valuesDependenciesSelected;
     const dependenciesSelectedToModify = dependenciesSelected.match(/\d+/g)
     const dependenciesSelectedParsed = dependenciesSelectedToModify.map(v => parseInt(v))
-    console.log(dependenciesSelectedParsed)
     deprecateCI(dependenciesSelectedParsed)
     setTimeout(() => {
-      initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+      initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
     }, 3500);
   })
 }
 
+const handleListSpecificCI = () => {
+  const data = getUpdatedData()
+  askToSelectOneDependencies(data, 'Select the CI you want to see their dependencies:').then(valuesDependenciesSelected => {
+    const { dependenciesSelected } = valuesDependenciesSelected;
+    const dependenciesSelectedToModify = dependenciesSelected.match(/\d+/g)
+    const dependenciesSelectedParsed = dependenciesSelectedToModify.map(v => parseInt(v))
+
+    const dependenciesCI = getDependenciesName(data[dependenciesSelectedParsed.toString()])
+    if (dependenciesCI.length > 0) {
+      console.log('Dependencies')
+      dependenciesCI.forEach((item) => {
+        console.log(`
+            |_____________________________________________
+            |                                           |_|
+            | CI Name: ${item}                        
+            |                                            _
+            |___________________________________________|_|
+      `)
+      })
+    } else {
+      console.log(`
+      _____________________________________________________________________
+     |                                 👎                                  |
+     |                                                                     |
+     |                 This CI doesn't have any dependencies               |                                              
+     |_____________________________________________________________________|
+  `)
+    }
+    setTimeout(() => {
+      initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
+    }, 3500);
+  })
+}
 // TODO: Simulate deprecate
-initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll);
+initialMenu(handleCreateCI, handleUpgradeCI, handleDeprecateCI, handleListAll, handleListSpecificCI);
+
 
